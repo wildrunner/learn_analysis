6f4b595f8be16ce3f34b465de22fb4ee0699b519
wildrunner <wildrunner@gmail.com>
2014-12-30 14:46:23 +0900

    add

--- manipulation/explore.R
+++ manipulation/explore.R
@@ -8,3 +8,28 @@ summary(custdata)
 ind <- which(is.na(custdata$is.employed))
 custdata[ind,]
 
+# histogram
+library(ggplot2)
+ggplot(custdata) + geom_histogram(aes(x=age), binwidth=5, fill="gray")
+
+# density plot
+library(scales)
+ggplot(custdata) + geom_density(aes(x=income)) + scale_x_continuous(labels=dollar)
+
+# log-scaled density plot
+ggplot(custdata) + geom_density(aes(x=income)) + scale_x_log10(breaks=c(100,1000,10000,100000), labels=dollar) + annotation_logticks(sides="bt")
+
+# bar chart
+ggplot(custdata) + geom_bar(aes(x=marital.stat), fill="gray")
+
+# horizontal bar chart
+ggplot(custdata) + geom_bar(aes(x=state.of.res), fill="gray") + coord_flip() + theme(axis.text.y=element_text(size=(rel(0.8))))
+
+# bar chart with sorted categories
+statesums <- table(custdata$state.of.res)
+statef <- as.data.frame(statesums)
+colnames(statef) <- c("state.of.res", "count")
+summary(statef)
+statef <- transform(statef, state.of.res=reorder(state.of.res, count))
+ggplot(statef) + geom_bar(aes(x=state.of.res, y=count), stat="identity", fill="gray") + coord_flip() + theme(axis.text.y=element_text(size=(rel(0.8))))
+
